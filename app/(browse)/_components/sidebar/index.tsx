f5dc02b0937b4442dd950b2583c6babcde658c9c
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-05-12 17:12:05 -0500

    feat(sidebar): add sidebar with functionality to collapsed

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,10 @@
+import { Toggle } from './toggle';
+import { Wrapper } from './wrapper';
+
+export const Sidebar = (): React.ReactNode => {
+	return (
+		<Wrapper>
+			<Toggle />
+		</Wrapper>
+	);
+};
