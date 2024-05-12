760b0f3d1cb16cbe393b161d71bed44dd488fa8f
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-05-12 15:29:02 -0500

    feat(clerk webhook): add config clerk webhook

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,85 @@
+import { Webhook } from 'svix';
+import { headers } from 'next/headers';
+import type { WebhookEvent } from '@clerk/nextjs/server';
+import { db } from '@/lib/db';
+
+export async function POST(req: Request): Promise<Response> {
+	// You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+	const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+
+	if (!WEBHOOK_SECRET) {
+		throw new Error(
+			'Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
+		);
+	}
+
+	// Get the headers
+	const headerPayload = headers();
+	const svixId = headerPayload.get('svix-id');
+	const svixTimestamp = headerPayload.get('svix-timestamp');
+	const svixSignature = headerPayload.get('svix-signature');
+
+	// If there are no headers, error out
+	if (!svixId || !svixTimestamp || !svixSignature) {
+		return new Response('Error occured -- no svix headers', {
+			status: 400
+		});
+	}
+
+	// Get the body
+	const payload = await req.json();
+	const body = JSON.stringify(payload);
+
+	// Create a new Svix instance with your secret.
+	const wh = new Webhook(WEBHOOK_SECRET);
+
+	let evt: WebhookEvent;
+
+	// Verify the payload with the headers
+	try {
+		evt = wh.verify(body, {
+			'svix-id': svixId,
+			'svix-timestamp': svixTimestamp,
+			'svix-signature': svixSignature
+		}) as WebhookEvent;
+	} catch (err) {
+		console.error('Error verifying webhook:', err);
+		return new Response('Error occured', {
+			status: 400
+		});
+	}
+
+	const eventType = evt.type;
+
+	if (eventType === 'user.created') {
+		await db.user.create({
+			data: {
+				externalUserId: payload.data.id,
+				username: payload.data.username,
+				imageUrl: payload.data.image_url
+			}
+		});
+	}
+
+	if (eventType === 'user.updated') {
+		await db.user.update({
+			where: {
+				externalUserId: payload.data.id
+			},
+			data: {
+				username: payload.data.username,
+				imageUrl: payload.data.image_url
+			}
+		});
+	}
+
+	if (eventType === 'user.deleted') {
+		await db.user.delete({
+			where: {
+				externalUserId: payload.data.id
+			}
+		});
+	}
+
+	return new Response('', { status: 200 });
+}
